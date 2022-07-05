9dc9445d6d45e0dd469f15f41986d0a8c79e08b1
Matt van Voorst <mattvanvoorst@contentful.com>
2022-07-05 13:32:00 +0200

    Set up the basis for the cheatsheet calls

---
+++ pages/cheatsheet/index.js
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+
+import { apiClient } from '@/lib/contentfulApi';
+
+const Home = ({ data }) => {
+  const { title, highlightedPosts } = data.fields;
+
+  return (
+    <>
+      <h1>{title}</h1>
+      {highlightedPosts.map(item => {
+        return (
+          <Link key={item.sys.id} href={`/cheatsheet/post/${item.fields.slug}`}>
+            {item.fields.title}
+          </Link>
+        );
+      })}
+    </>
+  );
+};
+
+export const getServerSideProps = async () => {
+  try {
+    const homePageEntries = await apiClient.getEntries({
+      content_type: 'pageHomepage',
+    });
+
+    const homePageEntry = homePageEntries?.items?.[0];
+
+    if (!homePageEntry) {
+      return {
+        notFound: true,
+      };
+    }
+
+    return {
+      props: {
+        data: homePageEntry,
+      },
+    };
+  } catch (e) {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+export default Home;
---
+++ pages/cheatsheet/post/[slug].js
@@ -0,0 +1,66 @@
+import { apiClient } from '@/lib/contentfulApi';
+
+const BlogPost = ({ data }) => {
+  const { title } = data.fields;
+  return (
+    <>
+      <h1>{title}</h1>
+    </>
+  );
+};
+
+export const getStaticProps = async ({ params }) => {
+  const slug = params?.slug;
+
+  if (!slug) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const blogPostEntries = await apiClient.getEntries({
+    content_type: 'pageBlogPost',
+    'fields.slug[match]': slug,
+  });
+
+  const blogPostEntry = blogPostEntries?.items?.[0];
+
+  if (!blogPostEntry) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    revalidate: 60,
+    props: {
+      data: blogPostEntry,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const entries = await apiClient.getEntries({
+    content_type: 'pageBlogPost',
+  });
+
+  const paths =
+    entries.items
+      .map(entry =>
+        entry.fields?.slug
+          ? {
+              params: {
+                slug: entry.fields.slug,
+              },
+            }
+          : null,
+      )
+      .filter(Boolean) || [];
+
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+};
+
+export default BlogPost;
